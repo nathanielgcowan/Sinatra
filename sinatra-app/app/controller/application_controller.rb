027ecfabf55f25ea42977c23be4fcb641b712400
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-24 04:25:29 +0000

    Automatically backed up by Learn

--- sinatra-app/app/controller/application_controller.rb
+++ sinatra-app/app/controller/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < Sinatra::Base
   configure do 
     set :publich_folder, 'public'
     set :views, 'app/views'
-    # enable :sessions ##do for cookies ##perfer firefox
-    # set :session_secret, "carcollection" ##do for cookies perfer firefox
+    enable :sessions ##do for cookies ##perfer firefox
+    set :session_secret, "carcollection" ##do for cookies perfer firefox
   end 
   
   get '/' do 
