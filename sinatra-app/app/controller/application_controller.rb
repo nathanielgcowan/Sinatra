76b8ef1483fc18aa93d2afba5b72b3e368cea3aa
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-24 04:21:43 +0000

    Automatically backed up by Learn

--- sinatra-app/app/controller/application_controller.rb
+++ sinatra-app/app/controller/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
   configure do 
     set :publich_folder, 'public'
     set :views, 'app/views'
-    enable 
+    # enable :sessions
+    # set :session_secret, "carcollection"
   end 
   
   get '/' do 
