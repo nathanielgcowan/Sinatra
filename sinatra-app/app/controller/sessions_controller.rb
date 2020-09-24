b3f5d36da04f76f9755f9dd66b99da982f2e220f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-24 04:59:51 +0000

    Automatically backed up by Learn

--- sinatra-app/app/controller/sessions_controller.rb
+++ sinatra-app/app/controller/sessions_controller.rb
@@ -7,7 +7,9 @@ class SessionsController < ApplicationController
   
   post '/sessions' do
     #this routes is processing their login
+    #the goal is to get their data to session
     session [:email] = params[:email]
+    #then bring them to post
     redirect '/posts'
   end 
   
