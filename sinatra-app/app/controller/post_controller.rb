640084d7f63c4dd1eb0778e0ad8a42af536b8eca
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-24 05:16:19 +0000

    Automatically backed up by Learn

--- sinatra-app/app/controller/post_controller.rb
+++ sinatra-app/app/controller/post_controller.rb
@@ -9,7 +9,7 @@ class PostController < ApplicationController
   get '/post/new' do 
     #Checking if they are logged in
     #this is what they can't do unless they are logged in
-    # if !session[:email] #"if not email---redirect" 
+    # if !session[:email] #"if not email---redirect" ## (this is not semantic we put this in the ApplicationController)
       redirect "/login" #Redirecting if they are not
     else
       "A new post form" #Rendering if they are
