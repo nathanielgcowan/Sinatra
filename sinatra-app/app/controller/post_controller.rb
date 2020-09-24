dc06c42d8dc3b60c2f85a7f68b9001964b9184e7
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-24 05:07:24 +0000

    Automatically backed up by Learn

--- sinatra-app/app/controller/post_controller.rb
+++ sinatra-app/app/controller/post_controller.rb
@@ -3,7 +3,16 @@ class PostController < ApplicationController
   get '/posts' do 
     ##raise session[:email].inspect
     "You are logged in as #{session[:email]}"
+    "This is a list of publicllly available posts"
   end 
+  
+  get '/post/new' do 
+    #this is what they can't do unless they are logged in
+    if session[:email].empty?
+      redirect "/login"
+    else
+      "A new post form"
+    end
   
 end 
     
