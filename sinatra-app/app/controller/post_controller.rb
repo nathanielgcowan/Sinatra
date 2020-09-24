67f7b45aa01147b8e98ab053c07472da48c16805
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-24 05:17:52 +0000

    Automatically backed up by Learn

--- sinatra-app/app/controller/post_controller.rb
+++ sinatra-app/app/controller/post_controller.rb
@@ -28,6 +28,7 @@ class PostController < ApplicationController
     #Checking if they are logged in
     #this is what they can't do unless they are logged in
     # if !session[:email] #"if not email---redirect" ## (this is not semantic we put this in the ApplicationController)
+    if !logged_in?  
       redirect "/login" #Redirecting if they are not
     else
       "An edit post form" #Rendering if they are
